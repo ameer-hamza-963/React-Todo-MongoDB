1fb4a2ba55a38b84f26de2117654a3618aba2f91
Ameer Hamza <ameerhamza963@gmail.com>
2023-11-20 20:20:03 +0500

    docker file added

--- Web/src/lib/index.js
+++ Web/src/lib/index.js
@@ -1,8 +1,8 @@
 const dev = "http://localhost:4000";
 const prod = "https://mern-todo.surge.sh";
-
+const API_URL = process.env.API_URL
 export const baseURL =
   window.location.hostname.split(":")[0] === "localhost" ||
   window.location.hostname.includes("192")
-    ? dev
-    : prod;
+    ? API_URL
+    : API_URL;
