6970a9010f3cb77ce1783c6774d63fd15a661c96
Ameer Hamza <ameerhamza963@gmail.com>
2023-11-24 18:29:34 +0500

    compose file updated

--- Web/src/lib/index.js
+++ Web/src/lib/index.js
@@ -1,10 +1,11 @@
-const dev = "http://localhost:4000";
-const prod = "https://mern-todo.surge.sh";
-const API_URL = process.env.API_URL
+// const dev = "http://localhost:4000";
+// const prod = "https://mern-todo.surge.sh";
+// const API_URL = process.env.API_URL
 // export const baseURL =
 //   window.location.hostname.split(":")[0] === "localhost" ||
 //   window.location.hostname.includes("192")
 //     ? API_URL
 //     : API_URL;
 
-export const baseURL = process.env.API_URL || "http://localhost:4000"
+console.log('URL', process.env.REACT_APP_API_URL)
+export const baseURL = process.env.REACT_APP_API_URL || "http://localhost:4000"
